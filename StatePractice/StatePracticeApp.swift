07e0c4abce44e0fd90d64de3e7df4f545d309938
Francisco Casillas <chiccasillas@gmail.com>
2025-03-04 10:28:58 -0600

    Initial Commit

---
+++ StatePractice/StatePracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  StatePracticeApp.swift
+//  StatePractice
+//
+//  Created by Francisco Jean on 04/03/25.
+//
+
+import SwiftUI
+
+@main
+struct StatePracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
